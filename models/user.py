4473de6e23deed95ba0739a2037cc621e249e189
earvin-tech <wsl@Earvin>
2024-12-09 20:05:53 +1100

    Created user model and schema

---
+++ models/user.py
@@ -0,0 +1,18 @@
+from init import db, ma
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    f_name = db.Column(db.String(100), nullable=False)
+    l_name = db.Column(db.String(100), nullable=False)
+    dob = db.Column(db.Date, nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ("id","f_name", "l_name", "dob", "email")
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
