6b414d8b7449e4502588cf67b39a2fdea3cc9e4a
earvin-tech <wsl@Earvin>
2024-12-10 15:17:58 +1100

    Created model and schema for exercise entity

---
+++ models/exercise.py
@@ -0,0 +1,16 @@
+from init import db, ma
+
+class Exercise(db.Model):
+    __tablename__ = "exercises"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    name = db.Column(db.String(100), nullable=False, unique=True)
+    muscle_group = db.Column(db.String(100), nullable=False, unique=True)
+
+class ExerciseSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "name", "muscle_group")
+
+exercise_schema = ExerciseSchema()
+exercises_schema = ExerciseSchema(many=True)
