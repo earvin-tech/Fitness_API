24c02b6cf79c16d3c5c53008119cb178cb242a0c
earvin-tech <wsl@Earvin>
2024-12-10 13:34:04 +1100

    Implement update operation on user entity

--- controllers/user_controller.py
+++ controllers/user_controller.py
@@ -61,3 +61,28 @@ def delete_user(user_id):
         return {"message": f"User '{user.f_name} {user.l_name}' deleted successfully"}
     else:
         return {"message": f"user with ID {user_id} does not exist"}, 404
+    
+
+@users_bp.route("/<int:user_id>", methods=["PUT", "PATCH"])
+def update_user(user_id):
+    try:
+        stmt = db.select(User).filter_by(id=user_id)
+        user = db.session.scalar(stmt)
+
+        body_data = request.get_json()
+
+        if user:
+            user.f_name = body_data.get("f_name") or user.f_name
+            user.l_name = body_data.get("l_name") or user.l_name
+            user.dob = body_data.get("dob") or user.dob
+            user.email = body_data.get("email") or user.email
+
+            db.session.commit()
+
+            return users_schema.dump(user)
+        else:
+            return {"message": f"User with ID {user_id} does not exist"}, 404
+
+
+    except IntegrityError:
+        return {"message": "Email address is already in use"}, 409
