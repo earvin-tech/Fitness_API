683d89b8b96a1f9102cf9919302cab76cb74334c
earvin-tech <wsl@Earvin>
2024-12-11 12:14:09 +1100

    Create relationship fields with user and workout models

--- models/workout.py
+++ models/workout.py
@@ -1,3 +1,5 @@
+from marshmallow import fields # type:ignore
+
 from init import db, ma
 
 class Workout(db.Model):
@@ -10,9 +12,13 @@ class Workout(db.Model):
     workout_date = db.Column(db.Date, nullable=False)
     duration = db.Column(db.Interval, nullable=False)
 
+    user = db.relationship("User", back_populates="workouts")
+
 class WorkoutSchema(ma.Schema):
+    ordered=True
+    user = fields.Nested("UserSchema", only=["f_name", "l_name"])
     class Meta:
-        fields = ("id", "name", "user_id", "workout_date", "duration")
+        fields = ("id", "name", "user_id", "workout_date", "duration", "user")
 
 workout_schema = WorkoutSchema()
 workouts_schema = WorkoutSchema(many=True)
