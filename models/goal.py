2a9fe0dbee05a2203b2d8156fbbbe211ad2da785
earvin-tech <wsl@Earvin>
2024-12-12 19:48:09 +1100

    Created model and schema for goal entity

---
+++ models/goal.py
@@ -0,0 +1,26 @@
+from marshmallow import fields # type:ignore
+
+from init import db, ma
+
+class Goal(db.Model):
+    __tablename__ = "goals"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    name = db.Column(db.String(100))
+    exercise_id = db.Column(db.Integer, db.ForeignKey("exercises"), nullable=False)
+    user_id = db.Coluumn(db.Integer, db.ForeignKey("users"), nullable=False)
+    goal_weight = db.Column(db.Float, nullable=False)
+    status_achieved = db.Column(db.Boolean, nullable=False, default=False)
+
+    user = db.relationship("User", back_populates="goals")
+    exercise = db.relationship("Exercise", back_populates="goals")
+    
+class GoalSchema(ma.Schema):
+    user = fields.Nested("UserSchema", only=["f_name", "l_name"])
+    exercise = fields.Nest("ExerciseSchema", only=["name"])
+    class Meta:
+        fields = ("id", "name", "exercise_id", "user_id", "goal_weight", "status_achiieved", "user", "exercise")
+
+goal_schema = GoalSchema()
+goals_schema = GoalSchema(many=True)
